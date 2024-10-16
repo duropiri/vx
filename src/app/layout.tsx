803a1cf2bd616ca818bf71008adccb57dc68a6b6
Duro <duro.dp02@gmail.com>
2024-10-16 17:56:41 -0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from "next";
 import "./globals.scss";
 import { PreloaderProvider } from "@/contexts/PreloaderContext";
 import PageAnimatePresence from "@/components/HOC/PageAnimatePresence";
+
 export const metadata: Metadata = {
   title: "Virtual Xposure",
   description: "Meet the Gold Standard in Real Estate Marketing",
+  keywords:
+    "Virtual Xposure, real estate marketing, digital marketing for realtors, social media management, content creation, real estate branding, realtor growth strategies, social media strategy, video content creation, real estate content marketing, realtor lead generation, brand visibility, online real estate marketing, data-driven marketing, social media automation, media buying, CRM pipeline management, real estate sales support, client engagement, virtual marketing solutions",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -14,6 +18,59 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Virtual Xposure</title>
+        <meta
+          name="description"
+          content="Meet the Gold Standard in Real Estate Marketing"
+        />
+        <meta name="keywords" content="Virtual Xposure, real estate marketing, digital marketing for realtors, social media management, content creation, real estate branding, realtor growth strategies, social media strategy, video content creation, real estate content marketing, realtor lead generation, brand visibility, online real estate marketing, data-driven marketing, social media automation, media buying, CRM pipeline management, real estate sales support, client engagement, virtual marketing solutions" />
+        <meta charSet="" />
+        <meta name="viewport" content="" />
+        <meta name="robots" content="" />
+
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon.webp"
+        />
+        <link
+          rel="icon"
+          type="image/webp"
+          sizes="32x32"
+          href="/images/favicon.webp"
+        />
+        <link
+          rel="icon"
+          type="image/webp"
+          sizes="16x16"
+          href="/images/favicon.webp"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://virtualxposure.com" />
+        <meta property="og:title" content="Virtual Xposure" />
+        <meta
+          property="og:description"
+          content="Meet the Gold Standard in Real Estate Marketing"
+        />
+        <meta
+          property="og:image"
+          content="/images/social-media-share-preview.webp"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://virtualxposure.com" />
+        <meta name="twitter:title" content="Virtual Xposure" />
+        <meta
+          name="twitter:description"
+          content="Meet the Gold Standard in Real Estate Marketing"
+        />
+        <meta
+          name="twitter:image"
+          content="/images/social-media-share-preview.webp"
+        />
+      </head>
       <body className={`antialiased`}>
         <PreloaderProvider>
           <PageAnimatePresence>{children}</PageAnimatePresence>
