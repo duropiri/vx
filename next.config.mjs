da59f578f27fd5b984183736962c8d9d4250c329
Duro <duro.dp02@gmail.com>
2024-12-04 06:07:32 -0700

    case studies and ui changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true, // Helps catch potential issues early on
-  images: {
-    // Optimize image loading
-    // domains: ["your-image-domain.com"], // Allow loading images from specific domains
-    formats: ["image/avif", "image/webp"], // Use modern formats
-  },
   compress: true, // Enables gzip/brotli compression for better performance
   poweredByHeader: false, // Removes the "x-powered-by" header for better security
   swcMinify: true, // Uses the newer, faster SWC compiler for JavaScript minification
   images: {
     deviceSizes: [500, 800, 1080, 1600, 2000],
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: true, // Add this to help with Vercel deployment issues
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
