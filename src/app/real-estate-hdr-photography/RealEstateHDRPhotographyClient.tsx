c4cf0c94ec4aa19dcc06a94149024907fdfd8fc3
Duro <duro.dp02@gmail.com>
2024-12-05 16:57:55 -0700

    optimizations 6

--- src/app/real-estate-hdr-photography/RealEstateHDRPhotographyClient.tsx
+++ src/app/real-estate-hdr-photography/RealEstateHDRPhotographyClient.tsx
@@ -282,7 +282,7 @@ export const UnlimitedRightSection = () => (
   <div className="relative flex size-full flex-col sm:flex-row items-start justify-center gap-[3rem] sm:gap-[3.75rem] max-h-[40vh] overflow-hidden rounded-[1.875rem]">
     {/* Gradient */}
     <div className="absolute left-0 top-0 flex flex-col w-full h-[7.5rem] origin-top-left bg-gradient-to-t from-transparent to-white to-85% pointer-events-none rounded-t-[1.875rem] z-10" />
-    <div className="flex flex-col size-full items-center justify-center hover:scale-110 mt-[5rem] transition-all cursor-select-hover w-[25rem]">
+    <div className="relative flex flex-col size-full aspect-[73/128] items-center justify-center hover:scale-110 mt-[5rem] transition-all cursor-select-hover w-[25rem]">
       <Image
         data-speed={1.2}
         src={unlimitedImage}
