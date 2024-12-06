3661a4e0e474aad5d732590ada0aa1f616982739
Duro <duro.dp02@gmail.com>
2024-12-05 17:59:41 -0700

    optimizations 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
