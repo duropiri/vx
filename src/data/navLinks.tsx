ec468ed6f67ca0fd68e103c4257613658494b143
Durotimi Duro-Pirisola <duro@Durotimis-MacBook-Pro.local>
2025-07-15 21:37:15 -0600

    navbar no blank target

--- src/data/navLinks.tsx
+++ src/data/navLinks.tsx
@@ -71,7 +71,7 @@ export const HeaderLinks = [
           icon: ServiceIcons.skool,
           title: "Exclusive Community",
           href: "https://portal.virtualxposure.com/communities/groups/the-vx-collective/",
-          target: "_blank",
+          // target: "_blank",
           rel: "noopener noreferrer",
         },
       ],
